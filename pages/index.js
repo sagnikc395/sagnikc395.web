39ff039f910451e023ce18da2925d16447b0c4e0
sagnikc395 <sagnikchatterjee395@gmail.com>
2023-03-02 00:01:25 +0530

    fix: some ui fixes

--- pages/index.js
+++ pages/index.js
@@ -22,15 +22,21 @@ export default function Home({ allPostsData }) {
           <title>{sitetitle}</title>
         </Head>
         <section className='items-center flex flex-col p-4'>
-          <p className='text-2xl font-bold text-indigo-600'>
+          <p className='text-3xl font-bold text-indigo-600'>
             Sagnik&apos;s Blog{" "}
           </p>
-          <p className='text-lg  p-2 font-semi'>
-            I am a software developer who likes to create beautiful digital
-            products.
+          <p className='text-xl  p-4'>
+            I am a software developer who likes to create{" "}
+            <span className='bg-violet-500 pl-0.5 pr-0 text-gray-900'>
+              beautiful{" "}
+            </span>
+            digital products.
           </p>
         </section>
-        <h2 className='p-4 text-2xl underline text-indigo-600'> Blog Posts</h2>
+        <h2 className='p-4 text-2xl font-bold  text-indigo-600'>
+          {" "}
+          Blog Posts:
+        </h2>
         <ul className='p-2 justify-center space-y-3'>
           {allPostsData.map(({ id, date, title }) => (
             <li key={id} className='p-2 border-2 rounded-lg border-violet-500'>
